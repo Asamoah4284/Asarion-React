6dee1faee00c915b99a59d24bbd02b00c9ba05e7
Asamoah Richard <107297787+Asamoah4284@users.noreply.github.com>
2024-12-13 00:13:29 +0000

    Update modal.jsx

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -29,7 +29,7 @@ const Modal = ({ isOpen, onClose }) => {
         setEmailError('');
         setPhoneError('');
         try {
-            const response = await fetch('http://localhost:5000/api/clients', {
+            const response = await fetch('https://asarion-backend.onrender.com', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
